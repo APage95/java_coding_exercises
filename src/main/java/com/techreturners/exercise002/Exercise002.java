54d2d50bbab28481e8450ff76fdfabcb59a85e9f
APage95 <annabellemiriam@gmail.com>
2023-01-23 19:08:51 +0000

    feat: Exercise2 complete

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,14 +2,18 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        // Add your code here
-        return false;
+    public boolean isFromManchester(Person person) {
+        if (person.getCity().equals("Manchester")) {
+            return true;
+        }
+        else return false;
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        // Add your code here
-        return false;
+    public boolean canWatchFilm(Person person, int ageLimit) {
+        if (person.getAge() >= ageLimit){
+            return true;
+        }
+       else return false;
     }
     
 }
---
+++ src/main/java/com/techreturners/exercise002/Person.java
@@ -0,0 +1,21 @@
+package com.techreturners.exercise002;
+
+public class Person {
+
+   private String forename;
+   private String surname;
+   private String city;
+   private int age;
+
+  public Person (String forename, String surname, String city, int age) {
+      this.forename = forename;
+      this.surname = surname;
+      this.city = city;
+      this.age = age; }
+
+  public String getForename(){return forename;}
+  public String getSurname(){return surname;}
+  public String getCity(){return city;}
+  public int getAge(){return age;}
+
+}
