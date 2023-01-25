e50c2d3c6c6213574b8e60884c7a7cbb895d7cca
APage95 <annabellemiriam@gmail.com>
2023-01-25 18:38:57 +0000

    feat: Added IceCream class

---
+++ src/main/java/com/techreturners/exercise003/IceCream.java
@@ -0,0 +1,19 @@
+package com.techreturners.exercise003;
+
+public class IceCream {
+
+    private final String flavour;
+    private final int iceCreamCode;
+
+
+    public IceCream(String flavour, int iceCreamCode){
+        this.flavour = flavour;
+        this.iceCreamCode = iceCreamCode;
+    }
+
+
+    public String getFlavour() {return flavour;}
+
+    public int getCode() {return iceCreamCode;}
+
+}
